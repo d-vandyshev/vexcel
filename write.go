bfb3f827942c4648bb42d19411b0e1558ba7d6bc
div0 <d.vandyshev@ya.ru>
2014-10-23 10:33:36 +0600

    process width of cell

--- write.go
+++ write.go
@@ -182,6 +182,7 @@ xmlns:xdr="http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing"
 xmlns:x14="http://schemas.microsoft.com/office/spreadsheetml/2009/9/main"
 xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006">
 <dimension ref="A1:{{.MaxPosition}}"/>
+<cols><col min="1" max="3" width="33" bestFit="1" customWidth="1"/></cols>
 {{.SheetData}}
 </worksheet>
 `))
